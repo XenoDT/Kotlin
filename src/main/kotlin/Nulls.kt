2b84593851cb6e03cc28f305e396715b773f1577
Diego <diegogomezgascon@gmail.com>
2026-09-22 10:36:48 +0200

    método printName ahora si

--- src/main/kotlin/Nulls.kt
+++ src/main/kotlin/Nulls.kt
@@ -6,7 +6,8 @@ class Nulls {
 //        nombre.let { println((nombre)) }
 //        println("Nombre Desconocido")
 //    }
-    fun printName(nombre: String?) = nombre.let {(println(it))} ?: println("Nombre desconocido")
+//    fun printName(nombre: String?) = nombre.let {(println(it))} ?: println("Nombre desconocido")
+    fun printName(nombre: String?) = println(nombre ?: "Nombre desconocido")
     fun getLength(nombre: String?):Int? = nombre?.length
     fun nullOrfirst(numbers: List<Int>):Int? = numbers.firstOrNull()
 //    fun firstOrNull(number: List<Int>): Int? =  if (number.isEmpty()) null else number.first()
